2e63cbbe66cbcc40304de72759c8da7274d749c5
Dan Blondell <danblondell@gmail.com>
2017-10-22 03:52:33 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters_table.rb
+++ db/migrate/005_create_characters_table.rb
@@ -2,7 +2,7 @@ class Characters < ActiveRecord::Migration
   def change
     create_table :character do |t|
       t.string :name
-      t.string :show_id    
+      t.integer :show_id    
     end
   end
 end
