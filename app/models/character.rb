038c1dfe83d5dc93e39dc4b2b4b51708baa93f16
Dan Blondell <danblondell@gmail.com>
2017-10-22 04:31:24 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -7,7 +7,7 @@ class Character < ActiveRecord::Base
   end
 
   def build_show(name)
-    self.show = shows.find_or_create_by(name: name)
+    self.show = Show.find_or_create_by(name: name)
   end
 
   def build_network
